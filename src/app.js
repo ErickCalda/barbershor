820314a6a0ea1ee7b15f3674a85ea714479594b9
ErickCalda <patriciocalda17@gmail.com>
2025-08-01 02:14:52 -0500

    backend funcional FINAL

--- src/app.js
+++ src/app.js
@@ -128,13 +128,13 @@ if (process.env.NODE_ENV === 'development') {
   
 }
 
-app.use('/uploads/media', (req, res, next) => {
+app.use('/uploads/servicios', (req, res, next) => {
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 });
 
 // Servir estáticos desde la carpeta uploads/servicio
-app.use('/uploads/media', express.static('uploads/media'));
+app.use('/uploads/servicios', express.static('uploads/servicios'));
 // Middleware de parsing
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
